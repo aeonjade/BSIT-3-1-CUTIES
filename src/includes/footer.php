3bd748327ce9d9f8bfa41dd828e73e887802d333
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2024-11-30 05:58:56 +0800

    footer edit

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>CUTIES | BSIT 3-1</p>
-    <p class="group-name">NOTOPRON</p>
+    <p class="sides">CUTIES | BSIT 3-1</p>
     <p class="quote">"For the students, by the students."</p>
+    <p class="sides">NOTOPRON</p>
 </footer>
