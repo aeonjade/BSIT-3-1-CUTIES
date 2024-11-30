14b7caa6a485c71a960caa64581204691399bc3f
krtRthr <140688186+krtRthr@users.noreply.github.com>
2024-11-30 21:27:48 +0800

    okay na ang login

--- src/logIn.php
+++ src/logIn.php
@@ -7,39 +7,37 @@
   <link rel="shorthand icon" href="images/VITS LOGO.png" />
   <title>Login - VITS</title>
   <link rel="stylesheet" href="css/style.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+
 </head>
 
 
 <body>
   <?php include "includes/nav.php" ?>
 
-      <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-      </div>
+  <div class="background">
+    <div class="shape"></div>
+    <div class="shape"></div>
+  </div>
   <section class="vh-100" id="logIn-header">
     <div class="background">
       <div class="shape"></div>
       <div class="shape"></div>
     </div>
 
-      <form>
-        <h3>Login Here Cutie</h3>
+    <form>
+      <h3>Login Here Cutie</h3>
 
-        <label for="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+      <label for="username">Username</label>
+      <input type="text" placeholder="Email or Phone" id="username" />
 
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+      <label for="password">Password</label>
+      <input type="password" placeholder="Password" id="password" />
 
-        <button>Log In</button>
-      </form>
-    </section>
+      <button>Log In</button>
+    </form>
+  </section>
   </main>
   <?php include "includes/footer.php" ?>
 </body>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </html>
