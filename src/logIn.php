b2a3ede635473a4a3b58820bc51596067f710599
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2024-11-30 21:15:21 +0800

    Fix

--- src/logIn.php
+++ src/logIn.php
@@ -10,28 +10,33 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 </head>
 
-<body>
-  <section id="logIn-header">
-    <?php include "includes/nav.php" ?>
 
+<body>
+  <?php include "includes/nav.php" ?>
+
+      <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+      </div>
+  <section class="vh-100" id="logIn-header">
     <div class="background">
       <div class="shape"></div>
       <div class="shape"></div>
     </div>
 
-    <form>
-      <h3>Login Here Cutie</h3>
+      <form>
+        <h3>Login Here Cutie</h3>
 
-      <label for="username">Username</label>
-      <input type="text" placeholder="Email or Phone" id="username" />
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" />
 
-      <label for="password">Password</label>
-      <input type="password" placeholder="Password" id="password" />
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
 
-      <button>Log In</button>
-    </form>
-  </section>
-
+        <button>Log In</button>
+      </form>
+    </section>
+  </main>
   <?php include "includes/footer.php" ?>
 </body>
 
