bb67b75089d769a608c99b7a02df181057d61f98
Drew-sky <bautistaandrew0123@gmail.com>
2024-12-01 22:24:11 +0800

    hahgsgawga
    hello

--- src/index.php
+++ src/index.php
@@ -14,14 +14,117 @@
 
   <?php include "includes/nav.php" ?>
 
-  <section id="society">
-    <div class="text-box">
-      <h1>Valenzuela Information and Technology Society</h1>
-      <p>The one of the best Academic organization at Pamatasan ng Lungsod ng Valenzuela</p>
-      <a class="hero-btn" href="">Visit our Facebook Page</a>
-    </div>
-  </section>
+  <main>
+        <div class="banner">
+            <img src= "images\3-1pics\429418526_751298306966469_4919992842754078699_n.jpg" alt="Background Image" class="banner-image">
+            <div class="overlay">
+              <div class="text-content">
+                <h1>For the students,<br>by the students.</h1>
+                
+                <div class="buttons">
+                    <p>Insert introduction here. Insert introduction here. Insert introduction here.</p>
+                  <button class="btn">WHAT WE DO</button>
+                  <button class="btn">WHO WE ARE</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
+        <section class="developerss">
+            <p>FEATURES OF THE WEEK</p>
+        </section>
+
+        <section class="project-showcase">
+            <div class="container">
+                <!-- Content Section -->
+                <div class="content">
+                    <!-- Images Section -->
+                    <div class="images">
+                        <img src="images\aeons.jpg" alt="Image 1" class="side-image left">
+                        <img src="images\bretz.jpg" alt="Image 2" class="main-image">
+                        <img src="images\pongs.jpeg" alt="Image 3" class="side-image right">
+                    </div>
+    
+                    <!-- Text Section -->
+                    <div class="text-content">
+                        <h1>THIS IS THEIR PROJECT</h1>
+                        <h3>By Rome, Tite, Pepe, Burat, Burnek, Salsalito, Ratbu</h3>
+                        <p>
+                            The project description goes here. Add details to match the content style of the image.
+                        </p>
+                        <!-- Buttons Section -->
+                        <div class="buttons">
+                            <button class="btn btn-exhibits">Other Exhibits</button>
+                            <button class="btn btn-see-more">See More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+          
+        <section class="projecttoowan-showcase">
+            <div class="container">
+                <!-- Content Layout -->
+                <div class="content">
+                    <!-- Text Content -->
+                    <div class="text-content">
+                        <h1>THIS IS THEIR PROJECT</h1>
+                        <h3>By Rome, Tite, Pepe, Burat, Burnek, Salsalito, Ratbu</h3>
+                        <p>
+                            The project description goes here. Add relevant details for a professional look.
+                        </p>
+                        <!-- Buttons Section -->
+                        <div class="buttons">
+                            <button class="btn btn-exhibits">Other Exhibits</button>
+                            <button class="btn btn-see-more">See More</button>
+                        </div>
+                    </div>
+    
+                    <!-- Images Layout -->
+                    <div class="images">
+                        <img src="images\drews.jpg" alt="Image 1" class="side-image left">
+                        <img src="images\vics.jpg" alt="Image 2" class="main-image">
+                        <img src="images\zads.jpg" alt="Image 3" class="side-image right">
+                    </div>
+                </div>
+            </div>
+        </section>
+   
+        <section class="developersss">
+            <h2><center>CUTIES | Developer</h2>
+            <div class="developer-list">
+                <div class="developersss">
+                    <div class="circle">AB</div>
+                    <p>Andrew <br>Bautista</p>
+                </div>
+                <div class="developersss">
+                    <div class="circle">MB</div>
+                    <p>Michael <br>Bretana</p>
+                </div>
+                <div class="developersss">
+                    <div class="circle">VC</div>
+                    <p>Vincent<br>Centeno</p>
+                </div>
+                <div class="developersss">
+                    <div class="circle">ZO</div>
+                    <p>Zadkiel<br> Ondevilla</p>
+                </div>
+                <div class="developersss">
+                    <div class="circle">KP</div>
+                    <p>Kurt <br>Pantaleon</p>
+                </div>
+                <div class="developersss">
+                    <div class="circle">JV</div>
+                    <p>Jade<br> Vicentuan</p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    
+</body>
+</html>
 
 
   <?php include "includes/footer.php" ?>
