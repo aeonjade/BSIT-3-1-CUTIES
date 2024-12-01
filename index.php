db77bec734b248c7189275fe83ba3bec147db8bc
krtRthr <140688186+krtRthr@users.noreply.github.com>
2024-12-01 14:45:55 +0800

    login page sync with database auth

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,16 +25,13 @@
 
   <section id="society">
     <div class="text-box">
-      <h1>Valenzuela Information and Technology Society</h1>
+      <h1>Hello there CUTIEpie</h1><?php echo $user_data['username']; ?>
       <p>The one of the best Academic organization at Pamatasan ng Lungsod ng Valenzuela</p>
       <a class="hero-btn" href="">Visit our Facebook Page</a>
     </div>
   </section>
 
-
-
   <?php include "includes/footer.php" ?>
 
 </body>
-
 </html>
