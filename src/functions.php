14099fb4202cc574ebb986f5260350f7c412a6c3
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2024-12-03 16:38:28 +0800

    slight changes

--- src/functions.php
+++ src/functions.php
@@ -3,16 +3,13 @@
 function check_login($con)
 {
 
-	if(isset($_SESSION['user_id']))
-	{
+	if (isset($_SESSION['user_id'])) {
 
 		$id = $_SESSION['user_id'];
 		$query = "select * from users where user_id = '$id' limit 1";
 
-		$result = mysqli_query($con,$query);
-		if($result && mysqli_num_rows($result) > 0)
-		{
-
+		$result = mysqli_query($con, $query);
+		if ($result && mysqli_num_rows($result) > 0) {
 			$user_data = mysqli_fetch_assoc($result);
 			return $user_data;
 		}
@@ -21,24 +18,22 @@ function check_login($con)
 	//redirect to login
 	header("Location: login.php");
 	die;
-
 }
 
 function random_num($length)
 {
 
 	$text = "";
-	if($length < 5)
-	{
+	if ($length < 5) {
 		$length = 5;
 	}
 
-	$len = rand(4,$length);
+	$len = rand(4, $length);
 
-	for ($i=0; $i < $len; $i++) { 
+	for ($i = 0; $i < $len; $i++) {
 		# code...
 
-		$text .= rand(0,9);
+		$text .= rand(0, 9);
 	}
 
 	return $text;
