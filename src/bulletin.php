2b5a505d31b0d22ae121e773d4a8d1db36700810
Zadkiel-O <168411383+Zadkiel-O@users.noreply.github.com>
2024-12-03 16:36:36 +0800

    moved relevant css styles of bulletin into bulletin.css

--- src/bulletin.php
+++ src/bulletin.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bulletin</title>
+    <link rel="stylesheet" href="css/bulletin.css" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
 
